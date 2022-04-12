a691b660534617a02bcbdbe9c635daa1cf22fe8d
chickenman34234 <92672797+chickenman34234@users.noreply.github.com>
2022-04-12 23:25:57 +0200

    updated index.js

--- example-pack/index.js
+++ example-pack/index.js
@@ -4,13 +4,14 @@ const cmdPrefix = "!" //can be set to whatever you want
 
 // triggers whenver a chat is sent to chat
 world.events.beforeChat.subscribe(chat => {
-  //Splits the message alowing you to get each word of it eg. args[1] would be the second word (or first word after the cmd name)
+  //Splits the message allowing you to get each word of it eg. args[1] would be the second word (or first word after the cmd name)
   if(chat.message.startsWith(cmdPrefix)) {
     let args = chat.message.slice(cmdPrefix.length).trim().split(/\s+/);
     const cmd = args[0].replace(cmdPrefix, "")
+      //Splits the message allowing you to get each word of it eg. args[1] would be the second word (or first word after the cmd name)
   
   switch (cmd) {
-    case 'test': //what yoy type in chat
+    case 'test': //what you type in chat
     chat.cancel = true //stops the message from showing in chat
       world.getDimension('overworld').runCommand("say Hello World!")
       //runs command 
